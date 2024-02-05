fd6cb001629a37343c2ef445df2d8dbc35bc1cd5
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-02-06 00:20:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,22 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args, client));
 	}
 }
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Web sunucu
+
+app.get('/', (req, res) => {
+
+res.sendStatus(200);
+
+});
+
+app.listen(port, () => {
+
+console.log('Sunucu ${port} numaralı bağlantı noktasında yürütülüyor.");
+
+});
 
 client.login(ayarlar.token)
